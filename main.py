c930db88b924b04d40290b1141902cef7305827e
AllCirclesPresuppose <114363778+AllCirclesPresuppose@users.noreply.github.com>
2023-07-15 02:12:01 +0000

    created custom inputs

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-for i in range(200, 300, 20):
+print(
+    "lets do ranges! type a starting number, ending before number, and how much you want to increment by!"
+)
+
+start = int(input("Sart at: "))
+end = int(input("End before: "))
+increment = int(input("Increment between values: "))
+
+for i in range(start, end, increment):
     print(i)
